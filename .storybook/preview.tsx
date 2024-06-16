44aeeedfa3c13613d5d70984d61fd568c1e94b6b
billgil <bill.gilmore@helio.co>
2024-06-16 13:08:05 +0100

    docs(Storybook): take global theme setting

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
 import "../lib/styles/global.css";
+import "./styles.css";
 
 const preview: Preview = {
   decorators: [
